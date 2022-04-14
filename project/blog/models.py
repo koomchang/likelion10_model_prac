c85618d165e8486408e40c8583d359cb6c998e09
koomchang <leahpar0401@gmail.com>
2022-04-14 19:23:53 +0900

    update | django_model 기능 추가 구현

--- project/blog/models.py
+++ project/blog/models.py
@@ -4,6 +4,8 @@ class Blog(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField(default="")
     created_at = models.DateTimeField(auto_now_add = True)
+    update_at = models.DateTimeField(auto_now = True)
+    author = models.CharField(max_length=10, default="Name")
 
     def __str__(self):
-        return self.title
+        return self.title + " / " + self.author
